d89f5623a4bdf08f7b091d02540e07ab0c920300
Khadija Anees <khadijaaneesofficials@gmail.com>
2026-02-23 12:49:07 +0500

    update styling

--- script.js
+++ script.js
@@ -55,3 +55,55 @@ nlFormInline?.addEventListener("submit", (e) => {
   e.preventDefault();
   subscribe(nlEmailInline.value, nlMsgInline, nlBtnInline);
 });
+
+const heroTrack = document.getElementById("heroTrack");
+
+// Desktop images
+const deskImages = [
+  "./assets/hero/desk.jpg",
+  "./assets/hero/00.jpg",
+  "./assets/hero/NL.png",
+];
+
+// Mobile images
+const mobImages = [
+  "./assets/hero/01.jpg",
+  "./assets/hero/02.jpg",
+  "./assets/hero/03.jpg",
+];
+
+const isMobile = window.innerWidth <= 480;
+const images = isMobile ? mobImages : deskImages;
+
+// create slides
+images.forEach((img) => {
+  const slide = document.createElement("div");
+  slide.classList.add("hero-slide");
+  slide.style.backgroundImage = `url(${img})`;
+  heroTrack.appendChild(slide);
+});
+
+// ⭐ clone first slide (IMPORTANT)
+const firstClone = heroTrack.children[0].cloneNode(true);
+heroTrack.appendChild(firstClone);
+
+let current = 0;
+
+setInterval(() => {
+  current++;
+  heroTrack.style.transition = "transform 1s ease";
+  heroTrack.style.transform = `translateX(-${current * 100}%)`;
+
+  // smooth reset
+  if (current === images.length) {
+    setTimeout(() => {
+      heroTrack.style.transition = "none";
+      heroTrack.style.transform = "translateX(0)";
+      current = 0;
+    }, 1000); // same as transition duration
+  }
+}, 3000);
+
+window.addEventListener("resize", () => {
+  location.reload();
+});
